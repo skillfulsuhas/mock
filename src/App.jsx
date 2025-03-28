9b56eec9ff612a3a4ff916bb19979b7b4c50d4e0
suhas <skillfulsuhas@gmail.com>
2025-03-28 07:33:40 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import CreateVapiConversation from "./pages/create-vapi-conversation.jsx"
 import ViewVapiConversation from "./pages/view-vapi-conversation.jsx"
 import Insights from "./pages/insights.jsx"
 // import "./css/app.css"
-import "./css/global.css"
+// import "./css/global.css"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Page />} />
+      <Route path="/cu" element={<Page />} />
       <Route path="/about" element={<About />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/create" element={<CreateVapiConversation />} />
+      <Route path="/" element={<CreateVapiConversation />} />
       <Route path="/view/:id" element={<ViewVapiConversation />} />
       <Route path="/insights" element={<Insights />} />
       <Route path="*" element={<NotFound />} />
